1af811eda419270e42d181814997bca3a6072c97
Saul Kravitz <skravitz@mitre.org>
2019-12-20 17:41:06 -0500

    updated for CIMPL 6.10.3.... had to unmap HealthCareService.eligilibity.id

--- processSearchParams.sed
+++ processSearchParams.sed
@@ -1,4 +1,4 @@
 s#VhDir_sp_#Plannet_sp_#
 s#http://hl7.org/fhir/uv/vhdir/SearchParameter#http://hl7.org/fhir/us/davinci-pdex-plan-net/SearchParameter#
-s#http://hl7.org/fhir/uv/vhdir/StructureDefinition/contactpoint-viaintermediary#http://hl7.org/fhir/us/davinci-pdex-plan-net/StructureDefinition/plannet-CViaIntermediary-extension#g
+s#http://hl7.org/fhir/uv/vhdir/StructureDefinition/contactpoint-viaintermediary#http://hl7.org/fhir/us/davinci-pdex-plan-net/StructureDefinition/plannet-ViaIntermediary-extension#g
 s#draft#active#
--- removefilesandsed.sh
+++ removefilesandsed.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
-ruby massage.rb ../plannet-out10/fhir/guide
-cd ../plannet-out10
+ruby massage.rb ~/cimpl/plannet-out-11/fhir/guide
+pushd ~/cimpl/plannet-out-11
 rm fhir/extensions/plannet-CAvailableTime-extension.json
 rm fhir/extensions/plannet-TAvailableTime-extension.json
 rm fhir/extensions/plannet-IdentifierStatus-extension.json
-rm fhir/extensions/plannet-Proficiency-extension.json
 rm fhir/extensions/plannet-TViaIntermediary-extension.json
 rm fhir/extensions/plannet-GeoLocation-extension.json
 rm fhir/extensions/plannet-AllDay-extension.json
 rm fhir/extensions/plannet-AvailableEndTime-extension.json
 rm fhir/extensions/plannet-AvailableStartTime-extension.json
 rm fhir/extensions/plannet-DaysOfWeek-extension.json
-rm fhir/extension/plannet-Qualification-extension.json
-rm fhir/extension/plannet-Practitioner-Qualification-VHDIR-extension.json
+rm fhir/extensions/plannet-Practitioner-Qualification-VHDIR-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-CAvailableTime-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-TAvailableTime-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-IdentifierStatus-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-TViaIntermediary-extension.json
-rm fhir/guide/resources/structuredefinition-plannet-Proficiency-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-GeoLocation-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-AllDay-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-AvailableEndTime-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-AvailableStartTime-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-DaysOfWeek-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-Practitioner-Qualification-VHDIR-extension.json
-for i in fhir/guide/resources/structuredefinition-plannet-*.json; do sed -i .bak -f ~/cimpl/davinci-pdex-plan-net/renameextensions.sed $i; done
-sed -i .bak -f ~/cimpl/davinci-pdex-plan-net/renamevaluesets.sed fhir/guide/pages/valuesets.html
-cd ../davinci-pdex-plan-net
+for i in fhir/guide/resources/*.json; do sed -i .bak -f ~/git/pdex-plan-net-cimpl/renameextensions.sed $i; done
+for i in fhir/profiles/*.json; do sed -i .bak -f ~/git/pdex-plan-net-cimpl/renameextensions.sed $i; done
+sed -i .bak -f ~/git/pdex-plan-net-cimpl/renamevaluesets.sed fhir/guide/pages/valuesets.html
+rm fhir/guide/resources/*.bak
+rm fhir/profiles/*.bak
+rm fhir/guide/pages/*.bak
+popd
 
