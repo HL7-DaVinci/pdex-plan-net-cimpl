5033dec949061897779d8f722bb1b5d3df9257e3
Saul Kravitz <skravitz@mitre.org>
2019-12-23 11:46:05 -0500

    fixed examples

--- removefilesandsed.sh
+++ removefilesandsed.sh
@@ -6,20 +6,12 @@ rm fhir/extensions/plannet-TAvailableTime-extension.json
 rm fhir/extensions/plannet-IdentifierStatus-extension.json
 rm fhir/extensions/plannet-TViaIntermediary-extension.json
 rm fhir/extensions/plannet-GeoLocation-extension.json
-rm fhir/extensions/plannet-AllDay-extension.json
-rm fhir/extensions/plannet-AvailableEndTime-extension.json
-rm fhir/extensions/plannet-AvailableStartTime-extension.json
-rm fhir/extensions/plannet-DaysOfWeek-extension.json
 rm fhir/extensions/plannet-Practitioner-Qualification-VHDIR-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-CAvailableTime-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-TAvailableTime-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-IdentifierStatus-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-TViaIntermediary-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-GeoLocation-extension.json
-rm fhir/guide/resources/structuredefinition-plannet-AllDay-extension.json
-rm fhir/guide/resources/structuredefinition-plannet-AvailableEndTime-extension.json
-rm fhir/guide/resources/structuredefinition-plannet-AvailableStartTime-extension.json
-rm fhir/guide/resources/structuredefinition-plannet-DaysOfWeek-extension.json
 rm fhir/guide/resources/structuredefinition-plannet-Practitioner-Qualification-VHDIR-extension.json
 for i in fhir/guide/resources/*.json; do sed -i .bak -f ~/git/pdex-plan-net-cimpl/renameextensions.sed $i; done
 for i in fhir/profiles/*.json; do sed -i .bak -f ~/git/pdex-plan-net-cimpl/renameextensions.sed $i; done
